a52cfb811b6c5f0641bdd731c2b79052eeeae4f1
mstrabbi159-debug <mstrabbi159@gmail.com>
2026-05-28 02:04:22 +0600

    Update config.js

--- config.js
+++ config.js
@@ -17,19 +17,20 @@ module.exports = {
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5,
-  showProgressBar: false,  // Show progress bar in track embed
-  showVisualizer: false,  // Show visualizer on music card (disabled for low-memory optimization)
+  showProgressBar: true,  // Show progress bar in track embed
+  showVisualizer: true,  // Show visualizer on music card (disabled for low-memory optimization)
   generateSongCard: true,  // custom song card image, if false uses thumbnail
   metadataTag: true,  // If true, always show Song Details even when the card image is present
   lowMemoryMode: true,   // Performance optimizations for low-memory environments (512MB RAM)
   errorLog: "", 
   nodes: [
+
       {
       name: "GlaceYT",
       password: "glace",
       host: "de-01.strixnodes.com",
       port: 2010,
-      secure: false
+      secure: ture
     }
   ]
 }
